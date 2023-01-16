56222105ee5e5d97b1f97a6d93f9896ef9df0c83
Ashe <ashe@soaringnetwork.com>
2023-01-16 20:45:44 +0100

    Style Fixes

--- script.js
+++ script.js
@@ -30,9 +30,31 @@ window.onload = async () => {
   const itemTemplate = Handlebars.compile(itemSource);
 
   // Register handlebars capitalize function
-  Handlebars.registerHelper("capitalize", function (str) {
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  Handlebars.registerHelper("capitalize", function (input) {
+    var i, j, str, lowers, uppers;
+    str = input.replace(/([^\W_]+[^\s-]*) */g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  
+    // Certain minor words should be left lowercase unless 
+    // they are the first or last words in the string
+    lowers = ['A', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At', 
+    'By', 'For', 'From', 'In', 'Into', 'Near', 'Of', 'On', 'Onto', 'To', 'With'];
+    for (i = 0, j = lowers.length; i < j; i++)
+      str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'), 
+        function(txt) {
+          return txt.toLowerCase();
+        });
+  
+    // Certain words such as initialisms or acronyms should be left uppercase
+    uppers = ['Id', 'Tv'];
+    for (i = 0, j = uppers.length; i < j; i++)
+      str = str.replace(new RegExp('\\b' + uppers[i] + '\\b', 'g'), 
+        uppers[i].toUpperCase());
+  
+    return str;
   });
+  Handlebars.registerHelper("newline", function (a) { return a.replace("\\n", "\n") });
   Handlebars.registerHelper("is", function (a, b) { return a == b });
   Handlebars.registerHelper("bigger", function (a, b) { return a > b });
   Handlebars.registerHelper("biggerOrEqual", function (a, b) { return a >= b });
