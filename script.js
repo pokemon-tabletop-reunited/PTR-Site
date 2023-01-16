c36558ceddfa9822f2f2e63ab07631ee21d66426
Ashe <ashe@soaringnetwork.com>
2023-01-16 15:00:06 +0100

    Actual Base Release

--- script.js
+++ script.js
@@ -1,104 +1,216 @@
 function getFileContent(filePath) {
-    return new Promise((resolve, reject) => {
-        var req = new XMLHttpRequest();
-    
-        // Define parameters for request.
-        req.open('get', 'list.hbs', true);
-    
-        // Wait for request to complete.
-        req.onreadystatechange = function(){
-            if (req.readyState == 4 && req.status == 200){
-                // Execute callback function and parse variables.
-                resolve(req.response)
-            }
-        };
-    
-        // Send request.
-        req.send();
-    });
+  return new Promise((resolve, reject) => {
+    var req = new XMLHttpRequest();
+
+    // Define parameters for request.
+    req.open('get', filePath, true);
+
+    // Wait for request to complete.
+    req.onreadystatechange = function () {
+      if (req.readyState == 4 && req.status == 200) {
+        // Execute callback function and parse variables.
+        resolve(req.response)
+      }
+    };
+
+    // Send request.
+    req.send();
+  });
+}
+
+function getRandomItemFromList(list) {
+  return list[Math.floor((Math.random() * list.length))];
 }
 
 window.onload = async () => {
-    // compile the template
-    const listSource = await getFileContent('list.hbs');
-    const listTemplate = Handlebars.compile(listSource);
-    const itemSource = await getFileContent('item.hbs');
-    const itemTemplate = Handlebars.compile(itemSource);
+  // compile the template
+  const listSource = await getFileContent('list.hbs');
+  const listTemplate = Handlebars.compile(listSource);
+  const itemSource = await getFileContent('item.hbs');
+  const itemTemplate = Handlebars.compile(itemSource);
 
-    // Register handlebars capitalize function
-    Handlebars.registerHelper("capitalize", function (str) {
-        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  // Register handlebars capitalize function
+  Handlebars.registerHelper("capitalize", function (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  });
+  Handlebars.registerHelper("is", function (a, b) { return a == b });
+  Handlebars.registerHelper("bigger", function (a, b) { return a > b });
+  Handlebars.registerHelper("biggerOrEqual", function (a, b) { return a >= b });
+  Handlebars.registerHelper("and", function (a, b) { return a && b });
+  Handlebars.registerHelper("or", function (a, b) { return a || b });
+  Handlebars.registerHelper("not", function (a, b) { return a != b });
+  Handlebars.registerHelper("lpad", function (str, len, char) {
+    str = str.toString();
+    while (str.length < len) str = char + str;
+    return str;
+  });
+
+  // Load Data
+  const monData = JSON.parse(await $.get("https://raw.githubusercontent.com/pokemon-tabletop-reunited/PTR-Data/master/pokemon-data.json")).map(mon => { return { ...mon, compendium: "mons" }});
+  const movesData = JSON.parse(await $.get("https://raw.githubusercontent.com/pokemon-tabletop-reunited/PTR-Data/master/moves-data.json")).map(move => { return { ...move, compendium: "moves" }});
+  const abilitiesData = JSON.parse(await $.get("https://raw.githubusercontent.com/pokemon-tabletop-reunited/PTR-Data/master/abilities-data.json")).map(ability => { return { ...ability, compendium: "abilities" }});
+  const capabilitiesData = JSON.parse(await $.get("https://raw.githubusercontent.com/pokemon-tabletop-reunited/PTR-Data/master/capabilities-data.json")).map(capability => { return { ...capability, compendium: "capabilities" }});
+  const edgesData = JSON.parse(await $.get("https://raw.githubusercontent.com/pokemon-tabletop-reunited/PTR-Data/master/edges-data.json")).map(edge => { return { ...edge, compendium: "edges" }});
+  const featsData = JSON.parse(await $.get("https://raw.githubusercontent.com/pokemon-tabletop-reunited/PTR-Data/master/feats-data.json")).map(feat => { return { ...feat, compendium: "feats" }});
+  const pokeEdgesData = JSON.parse(await $.get("https://raw.githubusercontent.com/pokemon-tabletop-reunited/PTR-Data/master/poke-edges-data.json")).map(pokeEdge => { return { ...pokeEdge, compendium: "poke-edges" }});
+  const tmsData = JSON.parse(await $.get("https://raw.githubusercontent.com/pokemon-tabletop-reunited/PTR-Data/master/tms-data.json")).map(tm => { return { name: tm.move + " (tm)", tmNumber: tm.number, compendium: "tms" }});
+  
+  function getCompendium(name) {
+    switch (name) {
+      case "mons": return monData;
+      case "moves": return movesData;
+      case "abilities": return abilitiesData;
+      case "capabilities": return capabilitiesData;
+      case "edges": return edgesData;
+      case "feats": return featsData;
+      case "poke-edges": return pokeEdgesData;
+      case "tms": return tmsData;
+    }
+  }
+  function tmLinks() {
+    $("[data-tm-number]").on("click", function (event) {
+      const tm = event.currentTarget.dataset.tmNumber;
+      const tmName = tmsData.find(x => x.tmNumber == tm).name.replace("(tm)", "").trim();
+      const data = movesData.find(x => x.name == tmName);
+      const html = itemTemplate(data);
+      $(".data-view").replaceWith(html);
     });
+  }
 
-    // Load Data
-    const monData = JSON.parse(
-        await $.get(
-            "https://raw.githubusercontent.com/pokemon-tabletop-reunited/PTR-Data/master/pokemon-data.json"
-        )
-    );
-    const moveData = JSON.parse(
-        await $.get(
-            "https://raw.githubusercontent.com/pokemon-tabletop-reunited/PTR-Data/master/moves-data.json"
-        )
-    );
+  const monOptions = {
+    includeScore: true,
+    keys: [
+      { name: "_id", weight: 5 },
+      { name: "number", weight: 3 },
+      { name: "ptuNumber", weight: 1 },
+      { name: "Type", weight: 2 },
+    ],
+    threshold: 0.25,
+  };
+  const monFuse = new Fuse(monData, monOptions);
 
-    const monOptions = {
-        includeScore: true,
-        keys: [
-            { name: "_id", weight: 5 },
-            { name: "number", weight: 3 },
-            { name: "ptuNumber", weight: 1 },
-            { name: "Type", weight: 2 },
-        ],
-        threshold: 0.25,
-    };
-    const monFuse = new Fuse(monData, monOptions);
+  const moveOptions = {
+    includeScore: true,
+    keys: [
+      { name: "name", weight: 5 },
+      { name: "type", weight: 2 },
+      { name: "category", weight: 2 },
+      { name: "effect", weight: 1 },
+    ],
+    threshold: 0.25,
+  };
+  const moveFuse = new Fuse(movesData, moveOptions);
 
-    const moveOptions = {
-        includeScore: true,
-        keys: [
-            { name: "name", weight: 5 },
-            { name: "type", weight: 2 },
-            { name: "category", weight: 2 },
-            { name: "effect", weight: 1 },
-        ],
-        threshold: 0.25,
-    };
-    const moveFuse = new Fuse(moveData, moveOptions);
+  const abilitiesOptions = {
+    includeScore: true,
+    keys: [
+      { name: "name", weight: 5 },
+      { name: "frequency", weight: 1 },
+      { name: "effect", weight: 1 },
+    ],
+    threshold: 0.25,
+  };
+  const abilitiesFuse = new Fuse(abilitiesData, abilitiesOptions);
 
-    let delayTimer;
-    $(".jsonsearchinput").on("keydown", function (event) {
-        if (delayTimer) clearTimeout(delayTimer);
-        delayTimer = setTimeout(function () {
-            const input = event.currentTarget.value;
+  const capabilitiesOptions = {
+    includeScore: true,
+    keys: [
+      { name: "name", weight: 5 },
+      { name: "effect", weight: 1 },
+    ],
+    threshold: 0.25,
+  };
+  const capabilitiesFuse = new Fuse(capabilitiesData, capabilitiesOptions);
 
-            if (input.length > 2) {
-                const results = [
-                    ...monFuse.search(input),
-                    ...moveFuse.search(input)
-                ].sort((a, b) => a.score - b.score);
-                
-                const html = listTemplate({
-                    total: results.length > 12 ? 12 : results.length,
-                    items: results.length > 12 ? results.slice(0, 12).map(x => x.item) : results.map((x) => x.item),
-                });
+  const edgesOptions = {
+    includeScore: true,
+    keys: [
+      { name: "name", weight: 5 },
+      { name: "prerequisites", weight: 3 },
+      { name: "effect", weight: 1 },
+    ],
+    threshold: 0.25,
+  };
+  const edgesFuse = new Fuse(edgesData, edgesOptions);
 
-                $("#searchresults").replaceWith(html);
-                $(".result-item").on("click", function (event) {
-                    const { name, id } = event.currentTarget.dataset;
-                    const data = id > 0 ? monData.find((x) => x.ptuNumber == id) : moveData.find((x) => x.name == name);
-                    $(".data-view").replaceWith(itemTemplate(data));
-                })
+  const featsOptions = {
+    includeScore: true,
+    keys: [
+      { name: "name", weight: 5 },
+      { name: "prerequisites", weight: 3 },
+      { name: "frequency", weight: 1 },
+      { name: "effect", weight: 1 },
+    ],
+    threshold: 0.25,
+  };
+  const featsFuse = new Fuse(featsData, featsOptions);
 
-                if (results.length == 1) {
-                    $(".result-item").click();
-                }
-            }
+  const pokeEdgesOptions = {
+    includeScore: true,
+    keys: [
+      { name: "name", weight: 5 },
+      { name: "effect", weight: 1 },
+    ],
+    threshold: 0.25,
+  };
+  const pokeEdgesFuse = new Fuse(pokeEdgesData, pokeEdgesOptions);
 
-            if (input.length < 3) {
-                $("#searchresults").hide();
-            }
-        }, 250);
-    });
+  const tmsOptions = {
+    includeScore: true,
+    keys: [
+      { name: "tmNumber", weight: 2 },
+      { name: "name", weight: 0.5 },
+    ],
+    threshold: 0.25,
+  };
+  const tmsFuse = new Fuse(tmsData, tmsOptions);
+
+  let delayTimer;
+  $(".jsonsearchinput").on("keydown", function (event) {
+    if (delayTimer) clearTimeout(delayTimer);
+    delayTimer = setTimeout(function () {
+      const input = event.currentTarget.value;
+
+      if (input.length > 1) {
+        const results = [
+          ...monFuse.search(input),
+          ...moveFuse.search(input),
+          ...abilitiesFuse.search(input),
+          ...capabilitiesFuse.search(input),
+          ...edgesFuse.search(input),
+          ...featsFuse.search(input),
+          ...pokeEdgesFuse.search(input),
+          ...tmsFuse.search(input),
+        ].sort((a, b) => a.score - b.score);
+
+        const html = listTemplate({
+          total: results.length > 12 ? 12 : results.length,
+          items: results.length > 12 ? results.slice(0, 12).map(x => x.item) : results.map((x) => x.item),
+        });
+
+        $("#searchresults").replaceWith(html);
+        $(".result-item").on("click", function (event) {
+          const { name, id, compendium } = event.currentTarget.dataset;
+          const data = getCompendium(compendium).find((x) => name ? x.name == name : x.ptuNumber == id);
+          const html = itemTemplate(data);
+          $(".data-view").replaceWith(html);
+          
+          tmLinks();
+        })
+
+        if (results.length == 1) {
+          $(".result-item").click();
+        }
+      }
+
+      if (input.length < 3) {
+        $("#searchresults").hide();
+      }
+    }, 250);
+  });
+
+  const list = getRandomItemFromList(["mons","moves", "abilities", "capabilities", "edges", "feats", "poke-edges", "tms"]); 
+  const item = getRandomItemFromList(getCompendium(list));
+  $(".data-view").replaceWith(itemTemplate(item));
+  tmLinks();
 };
-
